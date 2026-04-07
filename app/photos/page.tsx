d29454a3418a29a082083116f2a968aed0c34e3e
Rita Berglund <andoberr@oregonstate.edu>
2026-04-07 15:21:27 -0400

    added placeholder to empty pages

---
+++ app/photos/page.tsx
@@ -0,0 +1,7 @@
+export default function PhotosPage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-serif">Wedding Details Coming Soon</h1>
+    </div>
+  );
+}

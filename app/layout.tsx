ed10f505f797ed243348aa3a00d9fb76cc669a7e
Rita Berglund <andoberr@oregonstate.edu>
2026-04-07 17:23:49 -0400

    updating preview image

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,13 +9,23 @@ const defaultUrl = process.env.VERCEL_URL
   : "http://localhost:3000";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://wedding-website-mu-nine.vercel.app/'), // Replace with your actual Vercel URL
+  metadataBase: new URL('https://wedding-website-mu-nine.vercel.app/'), 
   title: "Rita & Wesley | October 24, 2026",
   description: "Join us for our wedding celebration in New York City.",
   openGraph: {
     title: "The Wedding of Rita & Wesley",
     description: "October 24, 2026 — New York City",
-    images: ["/images/363545_KW_306.jpg"], 
+    url: 'https://wedding-website-mu-nine.vercel.app/',
+    siteName: 'Rita and Wesley Wedding',
+    images: [
+      {
+        url: '/images/363545_KW_306.jpg', // Path relative to the public folder
+        width: 1200,
+        height: 630,
+        alt: 'Rita and Wesley under the Tokyo arch',
+      },
+    ],
+    locale: 'en_US',
     type: 'website',
   },
 };
